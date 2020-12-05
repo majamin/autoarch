4bdbbbc6abaaf9493ab2dac873d01d1d6ed16531
Marian Minar <majamin@gmail.com>
2020-12-05 12:10:50 -0800

    add multimedia cards to finddevs

--- .local/bin/finddevs.sh
+++ .local/bin/finddevs.sh
@@ -10,3 +10,14 @@ for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
         echo "/dev/$devname - $ID_SERIAL"
     )
 done
+
+for sysdevpath in $(find /sys/bus/mmc/devices/mm*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname="$(udevadm info -q name -p $syspath)"
+        [[ "$devname" == "bus/"* ]] && exit
+        eval "$(udevadm info -q property --export -p $syspath)"
+        [[ -z "$ID_SERIAL" ]] && exit
+        echo "/dev/$devname - $ID_SERIAL"
+    )
+done
