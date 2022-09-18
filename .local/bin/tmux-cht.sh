dcb0b098cf57e5010a5f2e1af013171cddeace5c
Marian Minar <majamin@gmail.com>
2022-09-18 08:07:27 -0700

    tmux cht.sh works now

--- .local/bin/tmux-cht.sh
+++ .local/bin/tmux-cht.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+
+languages="$HOME/.config/tmux/tmux-cht-languages"
+command="$HOME/.config/tmux/tmux-cht-command"
+
+selected=`cat "$languages" "$command" | fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.tmux-cht-languages; then
+if grep -qs "$selected" "$languages"; then
     query=`echo $query | tr ' ' '+'`
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
