0d7d411bd4276059640bea56b18563aeff857972
Marian Minar <majamin@gmail.com>
2022-09-26 10:14:51 -0700

    Hard code colors into script - less files

--- .local/bin/statusbar/bar.sh
+++ .local/bin/statusbar/bar.sh
@@ -6,7 +6,13 @@
 interval=0
 
 # load colors
-. ~/.config/chadwm/scripts/bar_themes/onedark
+black=#1e222a
+green=#7eca9c
+white=#abb2bf
+grey=#282c34
+blue=#7aa2f7
+red=#d47d85
+darkblue=#668ee3
 
 cpu() {
   cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
