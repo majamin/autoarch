feb071a9eca9bde7a458f1bbbd104ee5f795cdf3
Marian Minar <majamin@gmail.com>
2022-11-12 08:45:10 -0800

    add internet connection to bar

--- .config/sway/swaybar.sh
+++ .config/sway/swaybar.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # The Sway configuration file in ~/.config/sway/config calls this script.
 
 # uptime_formatted=$(uptime | cut -d ',' -f1  | cut -d ' ' -f4,5)
@@ -6,6 +6,18 @@ linux_version=$(uname -r | cut -d '-' -f1)
 # battery_status=$(cat /sys/class/power_supply/BAT0/status)
 date_formatted=$(date "+%a %F %H:%M")
 
+net() {
+  SSID=$(nmcli -g GENERAL.CONNECTION dev show | tr -d '[:space:]')
+  if [[ -z "$SSID" ]]; then
+    ICON='🖧'
+    SSID='eth'
+  else
+    ICON='󰤨'
+  fi
+
+  printf "$ICON %s" "$SSID"
+}
+
 # 💎 💻 💡 🔌 ⚡ 📁 ↑ 🔋\|
-printf "%s" "🐧$linux_version  $date_formatted"
+printf "%s" "🐧$linux_version   $(net)   $date_formatted"
 
