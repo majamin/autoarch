41b7c47dbfc84704f71b77962432b0dffcc18a98
Marian Minar <majamin@gmail.com>
2022-11-12 08:52:45 -0800

    slight change to date display on bar

--- .config/sway/swaybar.sh
+++ .config/sway/swaybar.sh
@@ -4,7 +4,7 @@
 # uptime_formatted=$(uptime | cut -d ',' -f1  | cut -d ' ' -f4,5)
 linux_version=$(uname -r | cut -d '-' -f1)
 # battery_status=$(cat /sys/class/power_supply/BAT0/status)
-date_formatted=$(date "+%a %F %H:%M")
+date_formatted=$(date "+(%a) %d-%b-%Y   %H:%M")
 
 net() {
   SSID=$(nmcli -g GENERAL.CONNECTION dev show | tr -d '[:space:]')
